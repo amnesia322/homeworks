5b9aea397f38a5c808761c2b4fe3c1682ecb673b
amnesia322 <dev.amnesia322@gmail.com>
2022-09-21 23:53:21 +0300

    HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import s from "./Header.module.css";
+import {PATH} from "./Pages";
+import {NavLink} from "react-router-dom";
+
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link}>Pre Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.link} >Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.link} >Junior Plus</NavLink>
+            <div className={s.block}></div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Junior = () => {
+    return (
+        <div>
+            Junior
+
+        </div>
+    )
+}
+
+export default Junior
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
